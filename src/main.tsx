a0a8057e320d33197aee85d7f075ca1d1e6679b7
unknown <hanikim2057@gmail.com>
2023-09-08 20:19:43 +0900

    chore: install styled components and set global styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from "react";
 
 import ReactDOM from "react-dom/client";
 
+import { GlobalStyles } from "@styles/global-styles.ts";
+
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: "@apis",
+        replacement: resolve(__dirname, "./src/apis"),
+      },
+      {
+        find: "@components",
+        replacement: resolve(__dirname, "./src/components"),
+      },
+      {
+        find: "@lib",
+        replacement: resolve(__dirname, "./src/lib"),
+      },
+      {
+        find: "@pages",
+        replacement: resolve(__dirname, "./src/pages"),
+      },
+      {
+        find: "@store",
+        replacement: resolve(__dirname, "./src/store"),
+      },
+      {
+        find: "@styles",
+        replacement: resolve(__dirname, "./src/styles"),
+      },
+      {
+        find: "@types",
+        replacement: resolve(__dirname, "./src/types"),
+      },
+    ],
+  },
+  plugins: [react(), tsconfigPaths()],
 });
