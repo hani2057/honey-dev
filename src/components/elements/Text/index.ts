efe55b8880989cf4ebed45eb75201aef2084628d
hani2057 <hanikim2057@gmail.com>
2023-11-21 23:40:39 +0900

    style: style selected gnb menu

--- src/components/elements/Text/index.ts
+++ src/components/elements/Text/index.ts
@@ -3,19 +3,14 @@ import { css, styled } from "styled-components";
 const Text = styled.p<{
   bold?: boolean;
   size?: number;
-  color?: string;
-  hColor?: string; // color when hovered
+  color?: string | null;
   pointer?: boolean;
 }>`
-  ${({ bold, size = 1, color, hColor, pointer }) => css`
+  ${({ bold, size = 1, color, pointer }) => css`
     font-size: ${size}rem;
     font-weight: ${bold ? "700" : "400"};
     color: ${color || "#000000"};
     cursor: ${pointer ? "pointer" : "auto"};
-
-    &:hover {
-      color: ${hColor || "#000000"};
-    }
   `}
 `;
 
--- src/features/gnb/components/GNBMenuText/index.tsx
+++ src/features/gnb/components/GNBMenuText/index.tsx
@@ -5,13 +5,19 @@ import { Text } from "@components/elements";
 interface GNBMenuTextProps {
   menu: string;
   path: string;
+  location: string;
 }
 
-export const GNBMenuText = ({ menu, path }: GNBMenuTextProps) => {
+export const GNBMenuText = ({ menu, path, location }: GNBMenuTextProps) => {
   const navigate = useNavigate();
 
   return (
-    <Text onClick={() => navigate(path)} hColor="blue" pointer={true}>
+    <Text
+      onClick={() => navigate(path)}
+      color={location === path ? "blue" : null}
+      bold={location === path}
+      pointer={true}
+    >
       {menu}
     </Text>
   );
