1fd0f95547121d98a9bc66218f02cae695d84f3d
hani2057 <hanikim2057@gmail.com>
2023-11-21 22:35:32 +0900

    chore: change name from "common" to "elements" for common components

--- src/components/elements/FlexDiv/index.ts
+++ src/components/elements/FlexDiv/index.ts
@@ -2,21 +2,28 @@ import { css, styled } from "styled-components";
 
 const FlexDiv = styled.div<{
   direction?: "column";
-  justify?: "start" | "end" | "space-between";
-  align?: "start" | "end";
+  justify?: "start" | "end" | "space-between"; // justify-content
+  align?: "start" | "end"; // align-items
   gap?: number;
+  px?: number; // padding-left && padding-right
+  py?: number; // padding-top && padding-bottom
+  p?: string; // padding
 }>`
   ${({
     direction = "row",
     justify = "center",
     align = "center",
     gap = 0,
+    px = 0,
+    py = 0,
+    p,
   }) => css`
     display: flex;
     flex-direction: ${direction};
     justify-content: ${justify};
     align-items: ${align};
     gap: ${gap}rem;
+    padding: ${p || `${py}rem ${px}rem`};
   `}
 `;
 
