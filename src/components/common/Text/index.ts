0d8dd57cefb50eb8c9c0d73d6de405c8e3562b7e
hani2057 <hanikim2057@gmail.com>
2023-11-21 21:57:35 +0900

    style: create Text common styled component

---
+++ src/components/common/Text/index.ts
@@ -0,0 +1,22 @@
+import { css, styled } from "styled-components";
+
+const Text = styled.p<{
+  bold?: boolean;
+  size?: number;
+  color?: string;
+  hColor?: string; // color when hovered
+  pointer?: boolean;
+}>`
+  ${({ bold, size = 1, color, hColor, pointer }) => css`
+    font-size: ${size}rem;
+    font-weight: ${bold ? "700" : "400"};
+    color: ${color || "#000000"};
+    cursor: ${pointer ? "pointer" : "auto"}
+
+    &:hover {
+      color: ${hColor || "#000000"};
+    }
+  `}
+`;
+
+export { Text };
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,2 +1,3 @@
 export * from "./FlexDiv";
 export * from "./Image";
+export * from "./Text";
