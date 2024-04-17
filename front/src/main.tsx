dff89bb136bd5397241b95779020236c66f93275
hani <hanikim2057@gmail.com>
2024-04-18 01:05:48 +0900

    feat: put QueryClientProvider in main.tsx

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,12 +3,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
+import { queryClient } from "@lib/reactQuery";
 import { router } from "@router/router";
 import { GlobalStyles } from "@styles/globalStyles.ts";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </QueryClientProvider>
 );
