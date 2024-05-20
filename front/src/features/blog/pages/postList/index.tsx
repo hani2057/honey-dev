8223f3a5af9e3cf03242d5c8c36dc5809f4751c5
hani <hanikim2057@gmail.com>
2024-05-20 22:02:33 +0900

    feat: integrate api for getting post lists

--- front/src/features/blog/pages/postList/index.tsx
+++ front/src/features/blog/pages/postList/index.tsx
@@ -1,46 +1,56 @@
+import { useState } from "react";
+
 import { useNavigate } from "react-router-dom";
 
+import { useGetPostList } from "@features/blog/api";
 import { Pagination } from "@features/blog/components";
+import { selectedCategoryIdAtom } from "@features/blog/store";
 import { PATH } from "@router/path";
 import { COLORS } from "@styles/colors";
+import dayjs from "dayjs";
+import { useAtomValue } from "jotai";
 
 import { FlexDiv, Text } from "@components/elements";
 
 import { PostCardDiv, PostContent, PostListWrapper } from "./style";
 
 export const PostList = () => {
+  const [page, setPage] = useState(1);
+  const categoryId = useAtomValue(selectedCategoryIdAtom);
   const navigate = useNavigate();
+
+  const req = { categoryId, page };
+  const { data } = useGetPostList(req);
+
+  if (!data) return <div>loading</div>;
+  const { totalPages, posts } = data;
+
+  // TODO: 페이지네이션에 추가
+  console.log(totalPages, setPage);
 
   return (
     <PostListWrapper>
-      {Array(10)
-        .fill(0)
-        .map((v) => (
-          <PostCardDiv
-            onClick={() => navigate(PATH.BLOG.POST.INDEX(1))}
-            key={v}
-          >
-            <FlexDiv direction="column" pWidth={75} align="start" gap={1}>
-              <Text size={1.25} bold={true} pointer={true}>
-                Post Title
-              </Text>
-              <PostContent>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Architecto optio eum numquam ducimus illo nulla ratione aperiam
-                quidem cum incidunt vero maxime dolor aspernatur consequatur
-                tempore odit, voluptatum repellendus esse?
-              </PostContent>
-            </FlexDiv>
-            <FlexDiv direction="column" gap={2}>
-              <Text size={0.75} color={COLORS.main[700]} line={1.25}>
-                TypeScript
-              </Text>
-              <Text size={0.75} line={1}>
-                2024.02.09.
-              </Text>
-            </FlexDiv>
-          </PostCardDiv>
-        ))}
+      {posts.map((post) => (
+        <PostCardDiv
+          onClick={() => navigate(PATH.BLOG.POST.INDEX(post.id))}
+          key={post.id}
+        >
+          <FlexDiv direction="column" pWidth={75} align="start" gap={1}>
+            <Text size={1.25} bold={true} pointer={true}>
+              {post.title}
+            </Text>
+            <PostContent>{post.content}</PostContent>
+          </FlexDiv>
+          <FlexDiv direction="column" gap={2}>
+            <Text size={0.75} color={COLORS.main[700]} line={1.25}>
+              {post.category}
+            </Text>
+            <Text size={0.75} line={1}>
+              {dayjs(post.createdAt).format("YYYY.MM.DD")}
+            </Text>
+          </FlexDiv>
+        </PostCardDiv>
+      ))}
       <Pagination />
     </PostListWrapper>
   );
