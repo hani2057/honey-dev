26d222ab3abee1147e307fc5b3779242c1b9d2c4
hani <hanikim2057@gmail.com>
2024-05-20 22:46:57 +0900

    bugfix: change blog title font to CDN for font application on Windows. (closes gh-2)

--- front/src/features/gnb/components/gnbHomeText/index.ts
+++ front/src/features/gnb/components/gnbHomeText/index.ts
@@ -3,7 +3,7 @@ import { styled } from "styled-components";
 import { Text } from "@components/elements";
 
 const GNBHomeText = styled(Text)`
-  font-family: "CookieRun-Regular";
+  font-family: "CookieRun";
   font-size: 1.3rem;
   cursor: pointer;
 `;
