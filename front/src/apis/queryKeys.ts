db2babcbc6f07f4df3b5e4af70c569150b159290
hani <hanikim2057@gmail.com>
2024-05-20 22:02:33 +0900

    feat: integrate api for getting post lists

---
+++ front/src/apis/queryKeys.ts
@@ -0,0 +1,9 @@
+import { GetPostsReq } from "@features/blog/api";
+
+export const QUERY_KEY = {
+  all: ["all"],
+  blog: {
+    all: () => [...QUERY_KEY.all, "posts"],
+    list: (request: GetPostsReq) => [...QUERY_KEY.blog.all(), "list", request],
+  },
+};
