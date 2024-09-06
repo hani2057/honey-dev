f3d2b774fd6b119693130092760e59ab6c39980e
hani <hanikim2057@gmail.com>
2024-09-06 15:11:09 +0900

    feat: integrate api for getting post detail

--- front/src/apis/queryKeys.ts
+++ front/src/apis/queryKeys.ts
@@ -5,5 +5,6 @@ export const QUERY_KEY = {
   blog: {
     all: () => [...QUERY_KEY.all, "posts"],
     list: (request: GetPostsReq) => [...QUERY_KEY.blog.all(), "list", request],
+    detail: (id: string) => [...QUERY_KEY.blog.all(), "detail", id],
   },
 };
