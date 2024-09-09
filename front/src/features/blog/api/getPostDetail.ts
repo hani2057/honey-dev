cb5b7aba452fe2508ef02d30882a86b1ceef71fc
hani <hanikim2057@gmail.com>
2024-09-09 15:35:00 +0900

    feat: add categoryId to GetPostDetailResponse DTO

--- front/src/features/blog/api/getPostDetail.ts
+++ front/src/features/blog/api/getPostDetail.ts
@@ -12,6 +12,7 @@ export type GetPostRes = {
   viewCnt: number;
   content: string;
   category: string;
+  categoryId: number;
   posts: {
     postId: number;
     title: string;
@@ -32,6 +33,7 @@ const getPostDetail = async (id: string): Promise<GetPostRes> => {
     content:
       "<h1>테스트용 제목</h1><p>이것저것 써 보자</p><p>테스트 테스트</p><p><br></p><p>이미지도 넣어 봐야 하는데</p><p><br></p><p>이렇게 쓰면 계속 추가되나?</p><p><br></p><h1>제목2</h1><p>이미지는 뺄까 봐... 아니면 에디터를 아예 위지윅이 아니라 마크다운으로 넣거나... 그게 낫지 않나? 어차피 내가 쓸 건데 예쁜 거 위지윅 필요 없고 그냥 편하게 쓰기 좋은 게 필요한 거니까.. 하지만 일 벌리지 말고 일단은 기본적인 거 먼저 해 두고 나중에 바꾸자 지금 중요한 건 일단 하는 거니까...</p><p><br></p><p>근데 여기에 내용 타이핑하는데 카테고리쪽까지 계속 페이지 전체가 리렌더링되네 왜 그러지 </p>",
     category: "TypeScript",
+    categoryId: 4,
     posts: [
       { postId: 1, title: "Post 1 Title", createdAt: new Date() },
       { postId: 2, title: "Post 2 Title", createdAt: new Date() },
