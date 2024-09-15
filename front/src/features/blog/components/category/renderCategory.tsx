ada77cc4ed2f833e2c473ddba5cd2d5b7d8d1a51
hani2057 <hanikim2057@gmail.com>
2024-09-15 18:19:45 +0900

    feat: 포스트 조회, 등록 페이지에서 사용할 카테고리 컴포넌트 분리 및 공통화

---
+++ front/src/features/blog/components/category/renderCategory.tsx
@@ -0,0 +1,63 @@
+import { Fragment } from "react";
+
+import { useGetCategories } from "@features/blog/api";
+import { TCategory, categoryType } from "@features/blog/types";
+
+import { FlexDiv, Text } from "@components/elements";
+
+import { CategoryDiv } from "./style";
+
+interface CategoryProps {
+  type: Extract<categoryType, "list" | "register">;
+  selectedCategoryId: number;
+  handleClickCategory: (categoryId: number) => void;
+}
+
+export function RenderCategory({
+  type,
+  selectedCategoryId,
+  handleClickCategory,
+}: CategoryProps) {
+  /**
+   * 카테고리 목록을 받아 JSX를 반환
+   *
+   * @param {TCategory[]} categoryList 카테고리 목록
+   * @returns children(하위 카테고리)이 있는 경우 하위 카테고리를 포함하여 JSX elements를 반환
+   */
+  const renderCategory = (categoryList: TCategory[]) =>
+    categoryList.map(({ categoryId, name, cnt, children }) => {
+      const isSelected = categoryId === selectedCategoryId;
+
+      if (children)
+        return (
+          <Fragment key={categoryId}>
+            <CategoryDiv $isSelected={isSelected} type={type}>
+              <Text
+                $pointer={true}
+                onClick={() => handleClickCategory(categoryId)}
+              >{`${name} (${cnt})`}</Text>
+            </CategoryDiv>
+            <FlexDiv
+              direction="column"
+              $align="start"
+              style={{ paddingLeft: "1rem" }}
+            >
+              {renderCategory(children)}
+            </FlexDiv>
+          </Fragment>
+        );
+      else
+        return (
+          <CategoryDiv $isSelected={isSelected} type={type} key={categoryId}>
+            <Text
+              $pointer={true}
+              onClick={() => handleClickCategory(categoryId)}
+            >{`${name} (${cnt})`}</Text>
+          </CategoryDiv>
+        );
+    });
+  const { data: categories } = useGetCategories();
+  if (!categories) return;
+
+  return renderCategory(categories);
+}
